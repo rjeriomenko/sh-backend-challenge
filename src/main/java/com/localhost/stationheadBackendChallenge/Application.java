650c56300e489cbe5fb5c0d9a3265ca62fb5f98f
Rokas Jeriomenko <rokas@jerio.me>
2024-04-06 10:54:54 -0400

    add spring boot setup

---
+++ src/main/java/com/localhost/stationheadBackendChallenge/Application.java
@@ -0,0 +1,13 @@
+package com.localhost.stationheadBackendChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
